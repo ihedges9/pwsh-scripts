81db1095c6faa0b973dd6f2dff11399f10144637
ihedges9 <42546907+ihedges9@users.noreply.github.com>
2024-07-10 08:41:48 -0400

    Update UIPathBotPackageCleanUp.ps1
    User scraping added, removes need for hardcoded list of bot names

--- UIPathBotPackageCleanUp.ps1
+++ UIPathBotPackageCleanUp.ps1
@@ -1,15 +1,12 @@
 # Remove WhatIf parameter from Line 20 to delete files
 
 # IN CASE MORE FOLDERS NEED ADDED, READ BELOW
-### Array for file paths of folders to remove excess packages is inside of the ForEach statement for users (Line 12)
-
-# Array for users for file path
-$users = @("UIPathtstbot1", "UIPathtstbot2")
+### Array for file paths of folders to remove excess packages is inside of the ForEach statement for users (Line 9)
 
 
 # Working code block
-ForEach ( $user in $users ) {
-    $paths = @("C:\Users\$user\.nuget\packages\uipath", "C:\Users\$user\.nuget\packages\uipath.system.activities", "C:\Users\$user\.nuget\packages\uipath.uiautomation.activities")
+gci "C:\Users" | ? { $_.Name -like "UIPath*bot*" } | ForEach-Object {
+    $paths = @("C:\Users\$_\.nuget\packages\uipath", "C:\Users\$_\.nuget\packages\uipath.system.activities", "C:\Users\$_\.nuget\packages\uipath.uiautomation.activities")
     ForEach ( $path in $paths ) {
         Get-ChildItem $path -File | 
                 # Sort them from Newest to Oldest
